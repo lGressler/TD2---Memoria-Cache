0697aa49299dd106075c873fd69c758dae258634
LucasGressler <lucasgresslersilva@gmail.com>
2025-06-29 23:46:04 -0300

    new main

--- main.c
+++ main.c
@@ -1,20 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "cache.h"
-
-int main()
+int main(int argc, char *argv[])
 {
 
-    int politica_escrita = 0;             // 0 = write-through, 1 = write-back
-    int tamanho_linha = 64;               // em bytes
-    int numero_linhas = 4096;             // número total de linhas da cache
-    int associatividade = 2;              // número de linhas por conjunto
-    int tempo_hit = 5;                    // tempo de acesso à cache (ns)
-    char politica_substituicao[] = "LRU"; // "LRU" ou "RAND"
-    int tempo_memoria = 70;               // tempo de acesso à memória principal (ns)
-
-    char arquivo_entrada[] = "teste.cache";
-    char arquivo_saida[] = "resultado.txt";
+    int politica_escrita = atoi(argv[1]);
+    int tamanho_linha = atoi(argv[2]);
+    int numero_linhas = atoi(argv[3]);
+    int associatividade = atoi(argv[4]);
+    int tempo_hit = atoi(argv[5]);
+    char *politica_substituicao = argv[6];
+    int tempo_memoria = atoi(argv[7]);
+    char *arquivo_entrada = argv[8];
+    char *arquivo_saida = argv[9];
 
     simular_cache(politica_escrita, tamanho_linha, numero_linhas, associatividade,
                   tempo_hit, politica_substituicao, tempo_memoria, arquivo_entrada, arquivo_saida);
